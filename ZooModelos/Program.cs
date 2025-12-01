df7bab3718f66895d3489e1970d898fcfdf5ed47
Sebastián Albacura <sebastianalbacura24@gmail.com>
2025-11-30 19:54:04 -0500

    Zoo

--- ZooModelos/Program.cs
+++ ZooModelos/Program.cs
@@ -8,6 +8,7 @@ namespace ZooModelos
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
             builder.Services.AddDbContext<ZooModelosContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("ZooModelosContext") ?? throw new InvalidOperationException("Connection string 'ZooModelosContext' not found.")));
 
@@ -15,6 +16,17 @@ namespace ZooModelos
             //fdgfdg  
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  policy =>
+                                  {
+                                      // **IMPORTANTE**: CAMBIAR ESTAS URLS por las de tu front-end en Render
+                                      policy.WithOrigins("http://localhost:3000", "https://tudominio.onrender.com")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                                  });
+            });
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services
